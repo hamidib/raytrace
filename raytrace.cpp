10b152923c2ac1b9712e989651828b850749d4b3
ruthobidah <ruthobidah@csu.fullerton.edu>
2017-10-25 21:54:36 -0700

    Added Group.cpp

--- raytrace.cpp
+++ raytrace.cpp
@@ -164,6 +164,7 @@ int main( int argc, char **argv ){
 
 #include <iostream>
 #include <string>
+#include "Group.h"
 #include "getopt.h"
 #include "Scene.h"
 #include "Ray.h"
@@ -294,51 +295,10 @@ vector<Ray> jig(float camWidth, float camHeight, float pixelSize, glm::vec3 camC
 }
 
 
-bool quadratic (float a, float b, float c, float x0, float x1) 
-{ 
-    float discr = b * b - 4 * a * c; 
-    if (discr < 0) return false; 
-    else if (discr == 0) x0 = x1 = - 0.5 * b / a; 
-    else { 
-        float q = (b > 0) ? 
-            -0.5 * (b + sqrt(discr)) : 
-            -0.5 * (b - sqrt(discr)); 
-        x0 = q / a; 
-        x1 = c / q; 
-    } 
-    if (x0 > x1) std::swap(x0, x1); 
- 
-    return true; 
-}
 
 
 
-//code/psuedo code for intersect of ray and sphere either we save the intercept to the depth file here or change the return type
-bool Group :: intercept ( glm::vec3 myRay, glm::vec3 dir, float r, float t){  //r=radius
-    float t0, t1;
-    float mult = -2.0;
-    float R = r * r;                                                     // radius^2
-    float a = glm::dot( dir , dir );                                     //where a = x^2 
-    
-                                                                     //for element we need to traverse the array of rays to do all these for each ray
-       
-    //glm::vec3 b0 = mult * dot(myRay * dir);                                       //where b = bx  
-    float b = mult * dot(myRay , dir); 
-    float c = (-1* R ) + glm::dot( myRay, myRay );                    //where c = c //r = radius //element = ray in vector container  
-    
-    if( !quadratic( a, b, c, t0, t1 ) ) return false;
-    if (t0 > t1) {                                       //the intercept that applies is the closest intercept
-        std::swap( t0, t1);
-    } 
-    if (t0 < 0) {
-        t0 = t1;                                         //if i0 is negative, then use i1
-        if (t0 < 0)                                      //if i1 is also negative, return false 
-        return false;
-    }
-    t= t0;    
-    return true;
 
-}
 
 /*
 Ray rayFactory(vector<glm::vec3> jiggy, glm::vec3 camDirection)
@@ -388,11 +348,11 @@ int main( int argc, char **argv ){
         {
             glm::vec3 myEle = element.returnPoint();
             cout << glm::to_string(myEle) << endl; //Ray object must refence private var inside to print
-            cout << "Intercept: " << myGroup.intercept(myEle, myCam._direction, myGroup._radius , myIntercept) <<endl;
+            cout << "Intercept: " << myGroup.intercept(myEle, myCam._direction, myGroup._center, myGroup._radius, myIntercept) <<endl;
             cout << "Image Pixel Orgins" << endl;
             cout << element.returnX() << " "<<element.returnY() << endl;
             //myImage.colorPixel(rayCount, rayCount, glm::vec3(0,0,1));//myImage.colorPixel(element.returnX(), element.returnY(), glm::vec3(1,1,1));//must use intercept and pixel color
-            if(myGroup.intercept(myEle, myCam._direction, myGroup._radius , myIntercept))
+            if(myGroup.intercept(myEle, myCam._direction, myGroup._center, myGroup._radius , myIntercept))
             {
                 //myImage.pixels[rayCount++] = Pixel(0,0,0);
                 //myImage.setPixel(i, j, 255, 0, 0, 255);
